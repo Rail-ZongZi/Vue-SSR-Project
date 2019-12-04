1f7fca9d7eb484b7c1161d945c3cc20b0ca57d04
粽子 <rail_0039@163.com>
2019-12-04 23:56:47 +0800

    项目webpack.config.js基本构建与Vue构建

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
 module.exports = {
-  
+  // 环境
+  module: 'production',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'build.js',
+    path: path.join(__dirname, 'dist')
+  }
 }
