6fd2e73cc209fd1fc2c31416a7a79b4cbeaf1b8e
Rail-ZongZi <rail_0039@126.com>
2020-05-01 11:58:41 +0800

    initialise project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Vue-SSR-Project
-主要是编写一些服务端渲染的项目与产品
+# mt-nuxt
+
+> My first-class Nuxt.js project
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+### 写法规范
+
+- 
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        mt-nuxt
+      </h1>
+      <h2 class="subtitle">
+        My first-class Nuxt.js project
+      </h2>
+      <div class="links">{{ message }}</div>
+    </div>
+    <div class="block">
+      <span class="demonstration">有默认值</span>
+      <el-color-picker v-model="color1"></el-color-picker>
+    </div>
+    <el-input placeholder="请输入内容"></el-input>
+    <el-drawer title="我是标题" :visible.sync="drawer" :with-header="false">
+      <span>我来啦!</span>
+    </el-drawer>
+    <el-switch
+      v-model="value"
+      active-color="#13ce66"
+      inactive-color="#ff4949"
+    ></el-switch>
+    <el-row>
+      <el-button plain>朴素按钮</el-button>
+      <el-button type="primary" plain>主要按钮</el-button>
+      <el-button type="success" plain @click="BtnCli">点击按钮</el-button>
+      <el-button type="info" plain>信息按钮</el-button>
+      <el-button type="warning" plain>警告按钮</el-button>
+      <el-button type="danger" plain>危险按钮</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'nuxt.js 测试案例',
+      color1: 'red',
+      value: true,
+      drawer: false
+    }
+  },
+  methods: {
+    BtnCli(event) {
+      console.log('Hello ' + event + '成功打印!')
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
